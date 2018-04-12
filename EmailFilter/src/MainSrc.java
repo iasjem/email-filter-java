9ee8e277fb5e1fab5d86a7ee83b025c9e6f08c76
iasjem <jemimah.beryl.sai@gmail.com>
2018-04-12 16:42:38 +0800

    Editting code for correction

--- EmailFilter/src/MainSrc.java
+++ EmailFilter/src/MainSrc.java
@@ -14,13 +14,19 @@ public class MainSrc {
 	private static String filterEmail(String email) {
 		
 		String[] emailSplit = email.split("@");	// split email provider from email address	
-		String filter =""; 
+		String filter =""; int i=0;
 		char[] arrEmail = emailSplit[0].toCharArray(); // domain name converts to character array
 		emailSplit[0] = ""; // initialize domain name
-		for (int i=0; i < arrEmail.length; i++) {
+
+		for ( i=1; i < arrEmail.length-1; i++) {
 			if (arrEmail[i] != '.') { arrEmail[i] = '*'; }
+		}  
+		
+		for(i=0; i< arrEmail.length; i++) {
 			emailSplit[0] += String.valueOf(arrEmail[i]); // convert character array to string
-		}  // concatenate string arrays with new values
+		}
+		
+		// concatenate string arrays with new values
 		email = filter.concat(emailSplit[0]).concat("@" + emailSplit[1]); 
 		return email;
 	}
